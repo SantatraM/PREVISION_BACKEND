ad24729e8c99b68c099b5052909a8c4b806749d8
SantatraM <santatryn@gmail.com>
2024-10-11 15:52:45 +0300

    seuil

--- previsionBack/crues/modeles/seuil.py
+++ previsionBack/crues/modeles/seuil.py
@@ -22,6 +22,16 @@ class Seuil(models.Model):
             return result if result else []
         except Exception as e:
             raise Exception(f"Error: {e}")
+        
+    def get_seuil_by_id_station(self,idstation):
+        try:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM v_seuil where idstation = %s"
+                cursor.execute(sql)
+                result = dispatchall(cursor,[idstation])
+            return result[0] if result else []
+        except Exception as e:
+            raise Exception(f"Error: {e}")
             
     def insert_seuil(self,idstation):
         try:
@@ -40,3 +50,12 @@ class Seuil(models.Model):
                 return True
         except Exception as e:
             raise Exception(f"Error updating seuil: {e}")
+        
+    def delete_seuil(self):
+        try:
+            with connection.cursor() as cursor:
+                sql = "DELETE FROM seuil where idseuil= %s"
+                cursor.execute(sql,[self.idseuil])
+                return True
+        except Exception as e:
+            raise Exception(f"Error deleting seuil: {e}")
